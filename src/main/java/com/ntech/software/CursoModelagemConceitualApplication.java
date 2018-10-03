2b997f332e7d3bda00986f93f3be5ba8b70b38be
Lindemberg Oliveira <linndemberg@outlook.com>
2018-10-02 22:22:10 -0300

    Estado e Cidade

--- src/main/java/com/ntech/software/CursoModelagemConceitualApplication.java
+++ src/main/java/com/ntech/software/CursoModelagemConceitualApplication.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ntech.software.domain.Categoria;
+import com.ntech.software.domain.Cidade;
+import com.ntech.software.domain.Estado;
 import com.ntech.software.domain.Produto;
 import com.ntech.software.repositories.CategoriaRepository;
+import com.ntech.software.repositories.EstadoRepository;
+import com.ntech.software.repositories.CidadeRepository;
 import com.ntech.software.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,6 +25,10 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
@@ -45,6 +53,19 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlãndia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 }
